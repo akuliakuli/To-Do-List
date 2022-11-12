82a88ea72aa8825e015c1e08c5c7fc31b05323fe
akuliakuli <kabby889@gmail.com>
2022-11-12 13:06:51 +0400

    Deploy fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import  { React,useState, useEffect } from "react";
 import { Form } from "./components/Form";
 import { Header } from "./components/Header";
 import { Postitem } from "./components/Postitem";
